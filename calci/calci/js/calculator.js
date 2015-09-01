8855d3d284eebc9c11af76c7e329fa3dd91dbaff
unknown <abhandwalkar@TRAINING3.internal.tavisca.com>
2015-09-01 13:34:49 +0530

    calci commmited

---
+++ calci/calci/js/calculator.js
@@ -0,0 +1,32 @@
+﻿
+var Addition = function () {
+
+    var num1 = parseInt(document.getElementById('NumberOne').value);
+    var num2 = parseInt(document.getElementById('numberTwo').value);
+    var ans = num1 + num2;
+    document.getElementById('answer').value = ans;
+}
+
+var Substraction = function () {
+
+    var num1 = parseInt(document.getElementById('NumberOne').value);
+    var num2 = parseInt(document.getElementById('numberTwo').value);
+    var ans = num1 - num2;
+    document.getElementById('answer').value = ans;
+}
+
+var Multiplication = function () {
+
+    var num1 = parseInt(document.getElementById('NumberOne').value);
+    var num2 = parseInt(document.getElementById('numberTwo').value);
+    var ans = num1 * num2;
+    document.getElementById('answer').value = ans;
+}
+
+var Division = function () {
+
+    var num1 = parseInt(document.getElementById('NumberOne').value);
+    var num2 = parseInt(document.getElementById('numberTwo').value);
+    var ans = num1 / num2;
+    document.getElementById('answer').value = ans;
+}
